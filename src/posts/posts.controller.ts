71d5fbc5419947d447c6b0f2a1774e39da199863
송준섭 <wnstjq7979@gmail.com>
2023-11-03 11:21:18 +0900

    :sparkles: 예시 Post interface 작성 및 getPost 라우팅

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { PostsService } from './posts.service';
+
+interface Post {
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
+
+  @Get()
+  getPost(): Post {
+    return {
+      author: 'John',
+      title: 'My First Post',
+      content: 'Hello World!',
+      likeCount: 0,
+      commentCount: 0,
+    };
+  }
 }
