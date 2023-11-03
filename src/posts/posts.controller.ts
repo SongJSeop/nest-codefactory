5513a9ef67fe9ec5f4cef7d8d61c7a9c972e5dbf
송준섭 <wnstjq7979@gmail.com>
2023-11-03 12:46:10 +0900

    :sparkles: path Param으로 특정 id의 post 가져오는 라우팅 구현

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { PostsService } from './posts.service';
 
-interface Post {
+interface PostModel {
+  id: number;
   author: string;
   title: string;
   content: string;
@@ -9,18 +10,44 @@ interface Post {
   commentCount: number;
 }
 
+let posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'John',
+    title: 'My First Post',
+    content: 'Hello World!',
+    likeCount: 0,
+    commentCount: 0,
+  },
+  {
+    id: 2,
+    author: 'John',
+    title: 'My Second Post',
+    content: 'Hello World!',
+    likeCount: 0,
+    commentCount: 0,
+  },
+  {
+    id: 3,
+    author: 'John',
+    title: 'My Third Post',
+    content: 'Hello World!',
+    likeCount: 0,
+    commentCount: 0,
+  },
+];
+
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @Get()
-  getPost(): Post {
-    return {
-      author: 'John',
-      title: 'My First Post',
-      content: 'Hello World!',
-      likeCount: 0,
-      commentCount: 0,
-    };
+  getPosts(): PostModel[] {
+    return posts;
+  }
+
+  @Get('/:id')
+  getPost(@Param('id', ParseIntPipe) id: number): PostModel {
+    return posts.find((post) => post.id === id);
   }
 }
