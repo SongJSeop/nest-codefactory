db6f6c84094cc1bb6aef3aff7c1c1be504c7303a
송준섭 <wnstjq7979@gmail.com>
2023-11-04 14:28:30 +0900

    :wrench: dotenv 패키지 설치 및 DB 정보들 환경 변수 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { configDotenv } from 'dotenv';
+configDotenv();
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    PostsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: process.env.POSTGRES_PORT as unknown as number,
+      username: process.env.POSTGRES_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
